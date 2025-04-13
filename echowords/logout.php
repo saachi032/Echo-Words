88608561a680c77ae77a53818ad2daa3b5a7d5c7
Saachi Mishra <146729723+saachi032@users.noreply.github.com>
2025-04-13 23:07:42 +0530

    old delete

--- echowords/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset(); // Clear all session variables
-session_destroy(); // Destroy the session
-
-header("Location: home.php"); // Redirect to home.php
-exit();
-?>
